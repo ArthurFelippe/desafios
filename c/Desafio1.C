3af9a07d59705b0cd865c5353d841e060fa95ee2
Arthur Felippe <arthur@dio.me>
2022-04-18 20:12:51 -0300

    first commit

---
+++ c/Desafio1.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define pi 3.14
+
+main(){ 
+
+float a, r;
+
+printf("digite o valor de raio: ");
+scanf("%f", &r);
+a = pi * pow (r,2);
+printf("\n");
+printf("Valor da Area é: %.2f",a);
+
+}
