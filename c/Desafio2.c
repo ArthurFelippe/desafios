a15ea75336fa8b97078b97313a32c63d3f1d6139
Arthur Felippe <arthur@dio.me>
2022-04-18 20:25:16 -0300

    Adicionando desafio2

---
+++ c/Desafio2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define pi 3.14
+
+main(){
+
+    int x=15, y=5;
+    float soma, subtracao, divisao, multiplicacao;
+    
+printf("Selecione a operação");
+printf("\n");printf("\n");
+
+soma = x+y;
+subtracao = x-y;
+divisao = x/y;
+multiplicacao = x*y;
+
+printf("\n");
+printf("A soma de x+y = %f", soma);
+printf("\n");printf("A subtracao de x-y = %f", subtracao);
+printf("\n");printf("A divisao de x/y = %f", divisao);
+printf("\n");printf("A multiplicacao de x*y = %f", multiplicacao);
+    
+}
