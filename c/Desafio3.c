d1a4ca26ecb2e7854ce446e21739127846a3b586
Arthur Felippe <arthur@dio.me>
2022-04-18 20:27:34 -0300

    Adicionando desafio3

---
+++ c/Desafio3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+main(){
+    float cot, dolar, real;
+    
+    printf("Digite a cotação do dolar: ");
+    scanf("%f", &cot);printf("Digite o valor em dolar: ");
+    scanf("%f", &dolar);real = cot * dolar;
+    printf("Valor em real é: %3.0f \n", real);
+    
+}
