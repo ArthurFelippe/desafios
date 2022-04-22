9786559851880ac2ef13a7fa871921ee8be1d44c
Arthur Felippe <arthur@dio.me>
2022-04-22 13:27:24 -0300

    add challenge4.js

---
+++ js/lista1/Desafio4.js
@@ -0,0 +1,28 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+/*
+Desafio4- Construa um programa que:
+
+Leia 2 números inteiros A e B;
+
+Calcule o quadrado para cada um;
+
+Somar os dois;
+
+Mostrar os resultados.
+*/
+ 
+rl.question('Informe o valor de A: ', (a) => {
+    rl.question('Informe o valor de B: ', (b) => {
+      const quadradoA = Math.pow(a,2);
+      const quadradoB = Math.pow(b,2);
+      const soma = quadradoA + quadradoB;
+      console.log(`A soma do quadrado A(${quadradoA}) + quadrado B(${quadradoB}): ${soma}`);
+      rl.close();
+    });
+  });
+
