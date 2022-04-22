cc72382f1e812a4c92b5b9a74bf629641505632b
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-04-22 12:52:35 -0300

    add resolution challenge 2 - js

---
+++ js/lista1/Desafio2.js
@@ -0,0 +1,24 @@
+/*
+Desafio2- Construa um programa que:
+
+Armazene o valor 15 em uma variável X;
+
+Armazene o valor 5 em uma variável Y;
+
+Calcule a soma, subtração, divisão e multiplicação dos valores de X por Y;
+
+Escrever os resultados.
+*/
+
+const x = 15;
+const y = 5;
+
+const soma = x + y;
+const subtracao = x - y;
+const divisao = x / y;
+const multiplicacao = x * y;  
+
+console.log(`Soma: ${soma}, Subtração: ${subtracao}, Divisão: ${divisao}, Multiplicação: ${multiplicacao}`);
+
+//console.log("Soma: " + soma + ", Subtração: " + subtracao + ", Divisão: " + divisao + ", Multiplicação: " + multiplicacao);
+
