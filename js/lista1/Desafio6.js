b8a6e45e858f735c9c61f59f4aec609086d317d5
Arthur Felippe <arthur@dio.me>
2022-04-22 13:55:21 -0300

    add challenge6.js

---
+++ js/lista1/Desafio6.js
@@ -0,0 +1,28 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+}); 
+
+/*
+Desafio6- Elaborar um programa que calcule o salário de um funcionário:
+
+O usuário deverá entrar com o valor do salário;
+
+O percentual de desconto do INSS.
+
+*/
+
+rl.question('Valor do salário: ', (valorSalario) => {
+    rl.question('Valor de desconto do INSS: ', (descontoINSS) => {
+        /*let salarioDesconto = valorSalario * descontoINSS / 100;
+        let salarioFinal = valorSalario - salarioDesconto;*/
+        let salarioFinal = valorSalario * (1-(descontoINSS / 100));
+
+
+        console.log(`O salário do funcionário é: ${salarioFinal}`);
+      rl.close();
+    });
+  });
+
+
