21e6e6889c94cf2a0c6964ca926359ab50211aea
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-04-22 13:10:25 -0300

    add resolution challenge 3 - js

---
+++ js/lista1/Desafio3.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+/*
+Desafio3- Construa um programa que:
+
+Leia a cotação do dólar;
+
+Leia um valor em dólares;
+
+Converta esse valor para Real;
+
+Mostre o resultado.
+
+Fórmula: Valor_Real = cot*valor_dolar.
+
+*/
+
+rl.question('Informe a cotação do dólar: ', (cot) => {
+  rl.question('Informe o valor em dólares: ', (valor) => {
+    const real = cot * valor;
+    console.log(`O valor em reais é: ${real}`);
+    rl.close();
+  });
+});
